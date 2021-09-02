fdd92daf5e0a8ff29cb5a0ecbc9da41b936f2758
asgeirkh <asgeir.kristinn@gmail.com>
2021-09-02 10:32:58 +0000

    test

---
+++ max_int.py
@@ -0,0 +1,14 @@
+
+num_int = 0
+max_int = 0
+
+while num_int != 0:
+    num_int = int(input("Input a number: "))    # Do not change this line
+
+    if num_int <= 0:
+        continue
+
+    count += 1
+
+
+print("The maximum is", max_int)    # Do not change this line
